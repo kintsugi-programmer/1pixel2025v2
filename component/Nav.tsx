cc0f738253d201d5def63f9272a63b731a309b7a
Siddhant Bali <119037976+kintsugi-programmer@users.noreply.github.com>
2024-12-20 18:26:57 +0530

    Nav Skeleton SF

--- component/Nav.tsx
+++ component/Nav.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='max-w-full h-30 bg-[#131313] flex justify-center '>
+        <div>LogoNameHome</div>
+        <div className="flex justify-center space-x-2">
+            <Link href="/">About Us</Link>
+            <Link href="/">Past Editions</Link>
+            <Link href="/">Upcoming Events</Link>
+            <Link href="/">Our Sponsors</Link>
+            <Link href="/">Contact Us</Link>
+
+        </div>
+    </div>
   )
 }
 
