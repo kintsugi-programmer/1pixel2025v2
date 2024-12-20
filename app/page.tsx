fb7eaeb8b38e8d60389def4e6434dadb1c42bd81
Siddhant Bali <119037976+kintsugi-programmer@users.noreply.github.com>
2024-12-20 18:18:42 +0530

    Sections Skeletons Initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-
+import About from "@/component/About";
+import Contact from "@/component/Contact";
+import Events from "@/component/Events";
+import Footer from "@/component/Footer";
+import Hero from "@/component/Hero";
+import Nav from "@/component/Nav";
+import Past from "@/component/Past";
+import Tickets from "@/component/Tickets";
+import Sponsors from "@/component/Sponsors";
 export default function Home() {
   return (<>
-  Hello World
+  <Nav />
+  <Hero />
+  <About />
+  <Past />
+  <Events />
+  <Sponsors />
+  <Tickets />
+  <Contact />
+  <Footer />
   </>
    
   );
