bef356a6ac6494a67cbe8ab240f7191034f80e97
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2024-12-28 22:12:56 +0530

    Bundler Parcel Rust

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Enable static export
   trailingSlash: true, // Optional: ensures all routes have a trailing slash in the URL
-    images: {
-      unoptimized: true,
-      },
+    // images: {
+    //   unoptimized: true,
+    //   },
 };
 
 export default nextConfig;
